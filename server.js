17e3c012328e5de7d9929e83222cce295cba20c9
ramyar299 <ramyarhalkawt0@gmail.com>
2026-06-16 13:39:26 +0300

    first upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// store data (temporary)
+let submissions = [];
+
+// receive form data
+app.post("/submit", (req, res) => {
+    const data = req.body;
+
+    console.log("New Data:", data);
+
+    submissions.push(data);
+
+    res.json({
+        success: true,
+        message: "Data saved successfully!"
+    });
+});
+
+// view all data
+app.get("/data", (req, res) => {
+    res.json(submissions);
+});
+
+// start server
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
